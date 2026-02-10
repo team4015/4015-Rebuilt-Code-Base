6d21431a36a3a39eab247b4089ebe4b6ca2bdb66
FRC Team 4015 | Jag <jags4015@gmail.com>
2026-02-09 22:18:08 -0500

    Fix DriveConfig typing compatibility and add constants aliases

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
 
   public static final DriveConfig DRIVE = ConfigLoader.loadDriveConfig();
   
-  public static final class ModuleConstants{
+  public static final class ModuleConstants {
     public static final double kWheelDiameterMeters = Units.inchesToMeters(DRIVE.module.wheelDiameterInches);
     
     public static final double kDriveMotorGearRatio = 1.0 / DRIVE.module.driveGearRatio;
@@ -17,18 +17,22 @@ public final class Constants {
 
     public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
 
-    public static final double kTurningEncoderRot2Rad = kDriveMotorGearRatio * 2.0 * Math.PI;
+    public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2.0 * Math.PI;
 
     public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60.0;
 
-     public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60.0;
+    // Backward-compatible aliases for older references/typos.
+    public static final double kDriveEncoderRPMMeterPerSec = kDriveEncoderRPM2MeterPerSec;
+    public static final double kDriveencoderRPMMeterPerSec = kDriveEncoderRPM2MeterPerSec;
+
+    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60.0;
 
     public static final double kPTurning = DRIVE.module.turningKP; 
   }
 
   public static final class DriveConstants {
 
-    public static final double kTrackWidth = Units.inchesToMeters(DRIVE.dimensions.trachWidthInches);
+    public static final double kTrackWidth = Units.inchesToMeters(DRIVE.dimensions.trackWidthInches);
 
     public static final double kWheelBase = Units.inchesToMeters(DRIVE.dimensions.wheelBaseInches);
 
@@ -39,6 +43,41 @@ public final class Constants {
             new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
             new Translation2d(-kWheelBase / 2,  kTrackWidth / 2)
         );
+
+    public static final int kFrontLeftDriveMotorPort = DRIVE.motors.drive.frontLeft;
+    public static final int kFrontRightDriveMotorPort = DRIVE.motors.drive.frontRight;
+    public static final int kBackLeftDriveMotorPort = DRIVE.motors.drive.backLeft;
+    public static final int kBackRightDriveMotorPort = DRIVE.motors.drive.backRight;
+
+    public static final int kFrontLeftTurningMotorPort = DRIVE.motors.turning.frontLeft;
+    public static final int kFrontRightTurningMotorPort = DRIVE.motors.turning.frontRight;
+    public static final int kBackLeftTurningMotorPort = DRIVE.motors.turning.backLeft;
+    public static final int kBackRightTurningMotorPort = DRIVE.motors.turning.backRight;
+
+    public static final int kFrontLeftDriveAbsoluteEncoderPort = DRIVE.encoders.absolutePorts.frontLeft;
+    public static final int kFrontRightDriveAbsoluteEncoderPort = DRIVE.encoders.absolutePorts.frontRight;
+    public static final int kBackLeftDriveAbsoluteEncoderPort = DRIVE.encoders.absolutePorts.backLeft;
+    public static final int kBackRightDriveAbsoluteEncoderPort = DRIVE.encoders.absolutePorts.backRight;
+
+    public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = DRIVE.encoders.absoluteOffsetsRad.frontLeft;
+    public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = DRIVE.encoders.absoluteOffsetsRad.frontRight;
+    public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = DRIVE.encoders.absoluteOffsetsRad.backLeft;
+    public static final double kBackRightDriveAbsoluteEncoderOffsetRad = DRIVE.encoders.absoluteOffsetsRad.backRight;
+
+    public static final boolean kFrontLeftDriveEncoderReversed = DRIVE.encoders.driveReversed.frontLeft;
+    public static final boolean kFrontRightDriveEncoderReversed = DRIVE.encoders.driveReversed.frontRight;
+    public static final boolean kBackLeftDriveEncoderReversed = DRIVE.encoders.driveReversed.backLeft;
+    public static final boolean kBackRightDriveEncoderReversed = DRIVE.encoders.driveReversed.backRight;
+
+    public static final boolean kFrontLeftTurningEncoderReversed = DRIVE.encoders.turningReversed.frontLeft;
+    public static final boolean kFrontRightTurningEncoderReversed = DRIVE.encoders.turningReversed.frontRight;
+    public static final boolean kBackLeftTurningEncoderReversed = DRIVE.encoders.turningReversed.backLeft;
+    public static final boolean kBackRightTurningEncoderReversed = DRIVE.encoders.turningReversed.backRight;
+
+    public static final boolean kFrontLeftDriveAbsoluteEncoderReversed = DRIVE.encoders.absoluteReversed.frontLeft;
+    public static final boolean kFrontRightDriveAbsoluteEncoderReversed = DRIVE.encoders.absoluteReversed.frontRight;
+    public static final boolean kBackLeftDriveAbsoluteEncoderReversed = DRIVE.encoders.absoluteReversed.backLeft;
+    public static final boolean kBackRightDriveAbsoluteEncoderReversed = DRIVE.encoders.absoluteReversed.backRight;
 
     // Speed limits
     public static final double kPhysicalMaxSpeedMetersPerSecond = DRIVE.limits.maxSpeedMps;
@@ -51,7 +90,7 @@ public final class Constants {
   }
   
 
-  public static final class OIConstants{
+  public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
 
     public static final int kDriverYAxis = 1;
--- src/main/java/frc/robot/DriveConfig.java
+++ src/main/java/frc/robot/DriveConfig.java
@@ -1,45 +1,75 @@
 package frc.robot;
 
+import com.google.gson.annotations.SerializedName;
+
 public class DriveConfig {
     public Module module;
     public Dimensions dimensions;
     public Motors motors;
+
+    @SerializedName(value = "encoders", alternate = {"encoder"})
     public Encoders encoders;
+
     public Limits limits;
 
-    public static class Module{
+    public static class Module {
         public double wheelDiameterInches;
         public double driveGearRatio;
         public double turningGearRatio;
+
+        @SerializedName(value = "turningKP", alternate = {"tuningKP"})
         public double turningKP;
     }
 
-    public static class Dimensions{
-        public double trachWidthInches;
+    public static class Dimensions {
+        @SerializedName(value = "trackWidthInches", alternate = {"trachWidthInches"})
+        public double trackWidthInches;
+
         public double wheelBaseInches;
     }
 
-    public static class Motors{
-        public MotorGroup drive;
-        public MotorGroup turning;
+    public static class Motors {
+        public IntGroup drive;
+        public IntGroup turning;
     }
 
-    public static class MotorGroup{
+    public static class Encoders {
+        @SerializedName(value = "absolutePorts", alternate = {"absolutePort"})
+        public IntGroup absolutePorts;
+
+        @SerializedName(value = "absoluteOffsetsRad", alternate = {"absolutePortsOffsetRad", "absoluteOffsetRad"})
+        public DoubleGroup absoluteOffsetsRad;
+
+        public BooleanGroup driveReversed;
+        public BooleanGroup turningReversed;
+        public BooleanGroup absoluteReversed;
+    }
+
+    public static class IntGroup {
         public int frontLeft;
         public int frontRight;
         public int backLeft;
         public int backRight;
     }
 
-    public static class Encoders{
-        public MotorGroup absolutePort;
-        public MotorGroup absoluteOffsetsRad;
-        public MotorGroup driveReversed;
-        public MotorGroup turningReversed;
+    public static class DoubleGroup {
+        public double frontLeft;
+        public double frontRight;
+        public double backLeft;
+        public double backRight;
     }
-    
-    public static class Limits{
+
+    public static class BooleanGroup {
+        public boolean frontLeft;
+        public boolean frontRight;
+        public boolean backLeft;
+        public boolean backRight;
+    }
+
+    public static class Limits {
+        @SerializedName(value = "maxSpeedMps", alternate = {"maxSpeed"})
         public double maxSpeedMps;
+
         public double maxAngularSpeedRadPerSec;
         public double teleopSpeedScale;
     }
