5de3bed29f5dd784e416dd0bd491e80ad0c8c8dd
Gsahaj <sahaj06chawla@gmail.com>
2026-02-11 13:46:15 -0500

    Add program for offset values

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,64 +4,87 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.controls.RobotContainer;
 
 public class Robot extends TimedRobot {
-  
-  private RobotContainer robotContainer;
-  private Command autonomousCommand;
+    private RobotContainer robotContainer;
+    private Command autonomousCommand;
+    private final DriveConfig DRIVE = ConfigLoader.loadDriveConfig();
+    private AnalogInput frontLeft = new AnalogInput(DRIVE.encoders.absolutePorts.frontLeft);
+    private AnalogInput frontRight = new AnalogInput(DRIVE.encoders.absolutePorts.frontRight);
+    private AnalogInput backLeft = new AnalogInput(DRIVE.encoders.absolutePorts.backLeft);
+    private AnalogInput backRight = new AnalogInput(DRIVE.encoders.absolutePorts.backRight);
+    private ShuffleboardTab tab;
 
-  @Override
-  public void robotInit() {
-    robotContainer = new RobotContainer();
-  }
+    @Override
+    public void robotInit() {
+      robotContainer = new RobotContainer();
+      tab = Shuffleboard.getTab("Encoder Offsets");
 
-  @Override
-  public void robotPeriodic() {
+      tab.addNumber("Front Left Absolute Encoder", () -> getAbsoluteEncoderRad(frontLeft));
+      tab.addNumber("Front Right Absolute Encoder", () -> getAbsoluteEncoderRad(frontRight));
+      tab.addNumber("Back Left Absolute Encoder", () -> getAbsoluteEncoderRad(backLeft));
+      tab.addNumber("Back Right Absolute Encoder", () -> getAbsoluteEncoderRad(backRight));
+    }
+
+    public double getAbsoluteEncoderRad(AnalogInput absoluteEncoder){
+        //gets the voltages from the encoder then get the actual rail voltage which is divided to get the angle
+        double angle = absoluteEncoder.getVoltage() / RobotController.getCurrent5V();
+        angle *= 2.0 * Math.PI; //convert this angle into radians
+        return angle; //result the angles and multiples by 1 or -1 if the absolute encoder is reversed
+    }
+
+
+    @Override
+    public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-  }
+    }
 
-  @Override
-  public void autonomousInit() {
+    @Override
+    public void autonomousInit() {
     autonomousCommand = robotContainer.getAutonomousCommand();
 
     if (autonomousCommand != null) {
       CommandScheduler.getInstance().schedule(autonomousCommand);
     }
-  }
+    }
 
-  @Override
-  public void teleopInit() {
+    @Override
+    public void teleopInit() {
     if (autonomousCommand != null) {
       autonomousCommand.cancel();
     }
-  }
+    }
 
-  @Override
-  public void disabledInit() {
-  }
+    @Override
+    public void disabledInit() {
+    }
 
-  @Override
-  public void autonomousPeriodic() {
-  }
+    @Override
+    public void autonomousPeriodic() {
+    }
 
-  @Override
-  public void teleopPeriodic() {
-  }
+    @Override
+    public void teleopPeriodic() {
+    }
 
-  @Override
-  public void disabledPeriodic() {
-  }
+    @Override
+    public void disabledPeriodic() {
+    }
 
-  @Override
-  public void testInit() {
+    @Override
+    public void testInit() {
     CommandScheduler.getInstance().cancelAll();
-  }
+    }
 
-  @Override
-  public void testPeriodic() {
-  }
+    @Override
+    public void testPeriodic() {
+    }
 }
