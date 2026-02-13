9ba3050ef0a45961a20c5c93aceac62404653a42
3p1c_r <aakashpathak9876@gmail.com>
2026-02-13 16:07:44 -0500

    new branch for YAGSL code

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,92 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveDrive;
+import swervelib.SwerveInputStream;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  
+  
+  
+  final CommandXboxController cmdXboxCtrl = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  
+  
+  
+  double headingX = cmdXboxCtrl.getRightX();
+  double headingY = cmdXboxCtrl.getRightY();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    
+
+
+    Command driveCmd = drivebase.driveCommand(() -> cmdXboxCtrl.getLeftX(), () -> cmdXboxCtrl.getLeftY(), () -> cmdXboxCtrl.getRightX());
+    System.out.println("Reached ConfigureBindings in RobotContainer");
+    drivebase.setDefaultCommand(driveCmd);
+
+    cmdXboxCtrl.a().onTrue(drivebase.runOnce(() -> drivebase.toggleFieldOriented()));
+    
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    //cmdXboxCtrl.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
+
+  
+
+  
+                                   
+                                                            
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,144 @@
+package frc.robot.subsystems;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+public class SwerveSubsystem extends SubsystemBase {
+
+    double maximumSpeed = 4.75;
+
+
+    SwerveDrive swerveDrive;
+    double deadband = Constants.OperatorConstants.DEADBAND;
+    private final SlewRateLimiter xLimiter = new SlewRateLimiter(5);
+    private final SlewRateLimiter yLimiter = new SlewRateLimiter(5);
+    private final SlewRateLimiter turningLimiter = new SlewRateLimiter(5);        
+
+    public SwerveSubsystem(File directory) {
+        try {
+            swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Swerve Drive directory not found. Finish those config files!");
+        }
+
+        swerveDrive.zeroGyro();
+        
+    }
+
+    /**
+     * Command to drive the robot using translative values and heading as a
+     * setpoint.
+     *
+     * @param translationX Translation in the X direction.
+     * @param translationY Translation in the Y direction.
+     * @param headingX     Heading X to calculate angle of the joystick.
+     * @param headingY     Heading Y to calculate angle of the joystick.
+     * @return Drive command. NOT BEING USED */
+     
+    /*public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier headingX,
+            DoubleSupplier headingY) {
+
+        return run(() -> {
+
+            Translation2d rawInputs = new Translation2d(translationX.getAsDouble(), translationY.getAsDouble());
+            Translation2d scaledInputs = SwerveMath.scaleTranslation(rawInputs, 0.8);
+
+            // Make the robot move
+
+            ChassisSpeeds chassisSpeeds = swerveDrive.swerveController.getTargetSpeeds(
+                    scaledInputs.getX(),
+                    scaledInputs.getY(),
+                    headingX.getAsDouble(),
+                    headingY.getAsDouble(),
+                    swerveDrive.getOdometryHeading().getRadians(),
+                    swerveDrive.getMaximumChassisVelocity());
+
+            swerveDrive.driveFieldOriented(chassisSpeeds);
+        });
+    } 
+
+    /**
+     * Command to drive the robot using translative values and heading as angular
+     * velocity.
+     *
+     * @param translationX     Translation in the X direction.
+     * @param translationY     Translation in the Y direction.
+     * @param angularRotationX Rotation of the robot to set
+     * @return Drive command.
+     */
+    private boolean fieldOriented = false;
+
+    public void toggleFieldOriented(){
+        fieldOriented = !fieldOriented;
+    }
+
+    public boolean isFieldOriented(){
+        return fieldOriented;
+    }
+    
+    
+    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY,
+            DoubleSupplier angularRotationX) {
+            
+        System.out.println("Reached driveCommand");
+
+        return run(() -> {
+            double maxVelocity = swerveDrive.getMaximumChassisVelocity();
+            
+            //Getting the doublees from the double suppliers and multiplying them by their max velocity
+            double vxMetersPerSecond = translationX.getAsDouble() * maxVelocity;
+            double vyMetersPerSecond = translationY.getAsDouble() * maxVelocity;
+            double angularRotation = angularRotationX.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity();
+            
+            //Limit the slew rate of the speeds to prevent sporadic motion and apply a deadband
+            
+            vxMetersPerSecond = MathUtil.applyDeadband(xLimiter.calculate(vxMetersPerSecond),deadband);
+            vyMetersPerSecond = MathUtil.applyDeadband(yLimiter.calculate(vyMetersPerSecond),deadband);
+            angularRotation = MathUtil.applyDeadband(turningLimiter.calculate(angularRotation),deadband);
+
+            // Make the robot move
+            
+            //Rotation2d angularHeading = swerveDrive.getOdometryHeading();
+            //ChassisSpeeds chassisSpeeds = new ChassisSpeeds(vxMetersPerSecond,vyMetersPerSecond,angularRotation);
+            //ChassisSpeeds fieldRelativeSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(chassisSpeeds, angularHeading);
+
+            Translation2d translation = new Translation2d(vxMetersPerSecond,vyMetersPerSecond);
+            
+            
+            swerveDrive.drive(translation, angularRotation, fieldOriented, false);
+            
+            //swerveDrive.driveFieldOriented(fieldRelativeSpeeds);
+
+            System.out.println("In Run Method" + "vxMetersPerSecond:" + vxMetersPerSecond + "vyMetersPerSecond: " + vyMetersPerSecond);
+            //swerveDrive.drive(translation, angularRotation, true, false);
+            //ChassisSpeeds.fromFieldRelativeSpeeds(null, null);
+        });
+
+
+    }
+
+    public SwerveDrive getSwerveDrive(){
+        return swerveDrive;
+    }
+
+}
