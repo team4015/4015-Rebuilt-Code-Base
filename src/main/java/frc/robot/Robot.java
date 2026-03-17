0662b3af497391faeabc6de6c628bd558df1be7a
Aakash Pathak <aakashpathak9876@gmail.com>
2026-03-16 23:39:02 -0400

    yeah

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -27,6 +28,7 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     System.out.println("RobotContainer Constructor");
     robotContainer = new RobotContainer();
+
   }
 
   /**
@@ -37,10 +39,7 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
 
-  @Override
-  public void robotInit(){
-    
-  }
+  
 
   @Override
   public void robotPeriodic() {
@@ -49,6 +48,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    DriverStation.silenceJoystickConnectionWarning(true);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
